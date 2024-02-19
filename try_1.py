4e9f88c161b530ba693b4b72e275ae3d303ff6dd
Hytham <90168217+HythamXV@users.noreply.github.com>
2024-02-19 11:21:06 +0300

    Update try_1.py

--- try_1.py
+++ try_1.py
@@ -7,7 +7,7 @@ print(f"Current Working Directory: {os.getcwd()}")
 print(f"Directory Contents: {os.listdir()}")
 
 # Update the model path to the correct absolute path
-model_path = "C:/Users/osamh/TuProject/Final Project/mushbari-main/mushbari-main/RandomForestClassifier_model2.sav"
+model_path = "RandomForestClassifier_model2.sav"
 load_model=pkl.load(open(model_path,"br"))
 def load_model():
     try:
